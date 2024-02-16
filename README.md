2957ac98a908cfe67b5f45e8e1aa5123600ac198
Vahe Sevachyan <vscode13@gmail.com>
2024-02-16 15:37:42 -0800

    updated interpolation examples + readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-In this repo you will find helpful VueJS code snippets that show vue js concepts.
+Hello World!
+
+In this repo you will find helpful VueJS code snippets covering various topics and concepts.
+
 This is a reference guide im building for my self, but i think anyone interested in learning vue would find it helpful.
+
+I will begin with the options API, then move on to the composition API.
+
+If you have any suggestions or requests feel free to reach out.
+
+Navigate to the src/components folder where you will find all the code
--- src/components/InterpolationDataBinding.vue
+++ src/components/InterpolationDataBinding.vue
@@ -2,16 +2,21 @@
   <h1>{{ str }}</h1>
   <h2>{{ arr[3] }} is number {{ arr[0] }}!</h2>
   <h3>{{ obj.val }} make building apps fun</h3>
+  <p>Here i am linking to <a v-bind:href="url">url</a></p>
 </template>
 <script>
 export default {
   data() {
     return {
-      str: "Hello World!", // str is being referenced in the h1 tag to display the value,
-      arr: [1, 2, 3, "VueJs", "JavaScript"], //arr is being referenced in the h2 tag to display the value
-      obj: { val: "Frameworks", val2: "programming" }, //obj is being referenced in the h3 tag to display the value
+      str: "Hello World!", // str is being referenced in the h1 tag using the double curly braces to display the value,
+      arr: [1, 2, 3, "VueJs", "JavaScript"], //arr is being referenced in the h2 tag using the double curly braces to display the value
+      obj: { val: "Frameworks", val2: "programming" }, //obj is being referenced in the h3 tag using the double curly braces to display the value
+      url: "https://www.google.com", //url is being referenced in the anchor tag and is bound to the href attribute using v-bind
     };
   },
 };
 </script>
 <style scoped></style>
+
+//interpolation is how you output data stored in a value which can be a string,
+array, object or boolean.
