f5aae16d0b90db4119f9d8a198b3c191c1801e02
Vahe Sevachyan <vscode13@gmail.com>
2024-02-17 12:29:47 -0800

    updated methods example

--- src/components/InterpolationDataBinding.vue
+++ src/components/InterpolationDataBinding.vue
@@ -3,6 +3,7 @@
   <h2>{{ arr[3] }} is number {{ arr[0] }}!</h2>
   <h3>{{ obj.val }} make building apps fun</h3>
   <p>Here i am linking to <a v-bind:href="url">url</a></p>
+  <h3>{{ courseGoals() }}</h3>
 </template>
 <script>
 export default {
@@ -13,6 +14,16 @@ export default {
       obj: { val: "Frameworks", val2: "programming" }, //obj is being referenced in the h3 tag using the double curly braces to display the value
       url: "https://www.google.com", //url is being referenced in the anchor tag and is bound to the href attribute using v-bind
     };
+  },
+  methods: {
+    courseGoals() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return "Learn Vue!";
+      } else {
+        return "Start on Nuxt!";
+      }
+    },
   },
 };
 </script>
